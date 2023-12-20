21689edd6ca5bee58a4fde6c4e13498945170100
lance-pyles <36748284+lance-pyles@users.noreply.github.com>
2023-12-20 04:48:36 +0000

    main

--- app.py
+++ app.py
@@ -1,7 +1,16 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=["GET"])
 def hello_world():
     return render_template("index.html", title="Hello")
+
+@app.route("/API", methods=["GET"])
+def hello_world2():
+    data = { 
+            "Modules" : 15, 
+            "Subject" : "Data Structures and Algorithms", 
+        } 
+  
+    return jsonify(data)
